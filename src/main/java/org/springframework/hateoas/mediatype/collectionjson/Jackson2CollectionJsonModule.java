e51ee983c0930f890d4c1ac2010a12fa1a0cdebe
Kevin McCarpenter™️ <kevin@moderne.io>
2023-07-25 20:49:50 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=RGVmYXVsdA%3D%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/springframework/hateoas/mediatype/collectionjson/Jackson2CollectionJsonModule.java
+++ src/main/java/org/springframework/hateoas/mediatype/collectionjson/Jackson2CollectionJsonModule.java
@@ -524,7 +524,7 @@ public class Jackson2CollectionJsonModule extends SimpleModule {
 		@Override
 		@SuppressWarnings("null")
 		public boolean isEmpty(PagedModel<?> value) {
-			return value.getContent().size() == 0;
+			return value.getContent().isEmpty();
 		}
 
 		/*
@@ -654,7 +654,7 @@ public class Jackson2CollectionJsonModule extends SimpleModule {
 				Links merged = items.stream() //
 						.map(CollectionJsonItem::getLinks) //
 						.reduce(links, //
-								(left, right) -> left.merge(right), //
+								Links::merge, //
 								(left, right) -> right);
 
 				CollectionJsonItem<?> firstItem = items.get(0).withOwnSelfLink();
@@ -793,7 +793,7 @@ public class Jackson2CollectionJsonModule extends SimpleModule {
 		}
 	}
 
-	static abstract class CollectionJsonDeserializerBase<T extends CollectionModel<?>>
+	abstract static class CollectionJsonDeserializerBase<T extends CollectionModel<?>>
 			extends ContainerDeserializerBase<T> implements ContextualDeserializer {
 
 		private static final long serialVersionUID = 1007769482339850545L;
